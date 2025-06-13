b6f1ca37ec22dba748e816b5b9623385c3807770
SnakeGuitar <snakersito08@gmail.com>
2025-06-13 17:30:42 -0600

    Inicialización de ventanas de Estudiante
    También se implementaron ventanas necesarias para el caso de uso: Subir documento de prácticas.

--- src/main/java/sgpp/controlador/usuarios/estudiante/FXMLPrincipalEstudianteController.java
+++ src/main/java/sgpp/controlador/usuarios/estudiante/FXMLPrincipalEstudianteController.java
@@ -1,4 +1,34 @@
 package sgpp.controlador.usuarios.estudiante;
 
-public class FXMLPrincipalEstudianteController {
+import javafx.event.ActionEvent;
+import sgpp.utilidad.Utilidad;
+
+public class FXMLPrincipalEstudianteController implements javafx.fxml.Initializable {
+    private static final String RUTA_FXML_SUBIR_DOCUMENTO = "/sgpp/vista/usuarios/estudiante/documentopracticas/FXMLSubirDocumentoPracticas.fxml";
+    private static final String RUTA_FXML_CONSULTAR_AVANCE = "";
+    private static final String RUTA_FXML_LLENAR_AUTOEVALUACION = "";
+    private static final String RUTA_FXML_FORMATO_EVALUACION = "";
+    private static final String RUTA_FXML_SUBIR_REPORTE = "";
+
+    @Override
+    public void initialize(java.net.URL url, java.util.ResourceBundle rb) {
+        // Inicialización de la vista principal del estudiante
+    }
+
+    public void clicBtnReporte(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnFormatoEvaluacion(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnAutoevaluacion(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnAvance(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnDocumentos(ActionEvent actionEvent) {
+        Utilidad.crearEscenario(RUTA_FXML_SUBIR_DOCUMENTO, "Subir Documento de Prácticas");
+
+    }
 }
---
+++ src/main/java/sgpp/controlador/usuarios/estudiante/documentopracticas/FXMLSubirDocumentoPracticasController.java
@@ -0,0 +1,9 @@
+package sgpp.controlador.usuarios.estudiante.documentopracticas;
+
+public class FXMLSubirDocumentoPracticasController implements javafx.fxml.Initializable {
+
+    @Override
+    public void initialize(java.net.URL url, java.util.ResourceBundle rb) {
+        // Inicialización de la vista para subir documentos de prácticas
+    }
+}
