b11919cc1bba6e31fceb92329c5e10f697e8ec8f
AbelYong <zs23014089@estudiantes.uv.mx>
2025-06-15 15:46:27 -0600

    Creado paquete dominio y la clase ProyectoDM

---
+++ src/main/java/sgpp/dominio/ProyectoDM.java
@@ -0,0 +1,19 @@
+/*
+ * Autor original: Abel Hernandez Yong
+ * Último autor: Abel Hernandez Yong
+ * Fecha de creación: 15-06-2025
+ * Fecha de la última versión aprobada:S/F
+ * Fecha de la última modificación: 15-06-2025 15:54
+ * Descripción: Clase de dominio para la entidad Proyecto
+ */
+
+/*
+ * Estado: Creado
+ * Modificaciones:
+ */
+
+package sgpp.dominio;
+
+public class ProyectoDM {
+
+}
