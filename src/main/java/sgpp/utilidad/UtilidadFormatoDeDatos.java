e36c8fc85dd467fd7386b589e6f7796fed948ccc
AbelYong <zs23014089@estudiantes.uv.mx>
2025-06-15 16:46:59 -0600

    Creada clase Utilidad para formato de datos, movidos metodos de validacion de datos desde utilidad

---
+++ src/main/java/sgpp/utilidad/UtilidadFormatoDeDatos.java
@@ -0,0 +1,26 @@
+/*
+ * Autor original: Abel Yong
+ * Ultimo autor: Abel Yong
+ * Fecha de creación: 15-06-2025
+ * Fecha de la última versión aprobada:
+ * Fecha de la última modificación: 10-06-2025 16:42
+ * Descripción: Clase DAO para contener validaciones comunes de formato de datos.
+ */
+
+/*
+ * Estado: En progreso
+ * Modificaciones: movido esUnNumeroEntero
+ */
+
+package sgpp.utilidad;
+
+public class UtilidadFormatoDeDatos {
+    public static boolean esUnNumeroEntero(String numero) {
+        try {
+            Integer.parseInt(numero);
+            return true;
+        } catch (NumberFormatException nfex) {
+            return false;
+        }
+    }
+}
