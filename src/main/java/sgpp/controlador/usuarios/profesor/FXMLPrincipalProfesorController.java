58b3a374fd8da6b5fb27fdd818ac8d6ff27e3a93
SnakeGuitar <snakersito08@gmail.com>
2025-06-15 15:09:25 -0600

    Actualización de ventana principal de Profesor

--- src/main/java/sgpp/controlador/usuarios/profesor/FXMLPrincipalProfesorController.java
+++ src/main/java/sgpp/controlador/usuarios/profesor/FXMLPrincipalProfesorController.java
@@ -1,4 +1,23 @@
 package sgpp.controlador.usuarios.profesor;
 
-public class FXMLPrincipalProfesorController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXMLPrincipalProfesorController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    public void clicBtnExpedienteEstudiante(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnValidarDocumento(ActionEvent actionEvent) {
+    }
+
+    public void clicBtnValidarReporte(ActionEvent actionEvent) {
+    }
 }
