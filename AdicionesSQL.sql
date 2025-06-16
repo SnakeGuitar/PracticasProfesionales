b947312be062816e7886b4f35ce7263a487c74dc
AbelYong <zs23014089@estudiantes.uv.mx>
2025-06-15 21:02:55 -0600

    Agregadas tablas y vistas para generar oficios de asignacion

--- AdicionesSQL.sql
+++ AdicionesSQL.sql
@@ -52,3 +52,35 @@ UPDATE proyecto SET
 WHERE ID_Proyecto = ID_Proyecto_in;
 END
 //
+
+CREATE VIEW vista_estudiante_proyecto_ov_responsable AS
+SELECT
+    e.ID_Estudiante AS ID_Estudiante,
+    e.nombre AS nombre_estudiante,
+    e.matricula AS matricula,
+    o.ID_Org_Vinculada AS ID_Org_Vinculada,
+    o.nombre AS nombre_organizacion,
+    o.direccion AS direccion,
+    p.ID_Proyecto AS ID_Proyecto,
+    p.nombre AS nombre_proyecto,
+    p.fecha_inicio AS fecha_inicio,
+    r.ID_Responsable AS ID_Responsable,
+    r.nombre AS nombre_responsable,
+    r.puesto AS puesto_responsable,
+    r.departamento AS departamento_responsable
+FROM estudiante e
+         JOIN proyecto p ON e.ID_Proyecto = p.ID_Proyecto
+         JOIN organizacion_vinculada o ON p.ID_Org_Vinculada = o.ID_Org_Vinculada
+         JOIN responsable_tecnico r ON p.ID_Responsable = r.ID_Responsable;
+
+CREATE TABLE oficio_asignacion (
+    ID_Oficio INT AUTO_INCREMENT PRIMARY KEY,
+    ID_Periodo INT NOT NULL,
+    ID_Estudiante INT NOT NULL,
+    documento LONGBLOB NOT NULL,
+    FOREIGN KEY (ID_Periodo) REFERENCES periodo(ID_Periodo),
+    FOREIGN KEY (ID_Estudiante) REFERENCES estudiante(ID_Estudiante)
+);
+
+ALTER TABLE oficio_asignacion
+    ADD CONSTRAINT uq_estudiante_periodo UNIQUE (ID_Estudiante, ID_Periodo);
