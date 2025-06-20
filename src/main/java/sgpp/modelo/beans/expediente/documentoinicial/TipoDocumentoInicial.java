a2aefbac656c4901b22d6bf0b827688180f226a6
SnakeGuitar <snakersito08@gmail.com>
2025-06-20 13:52:43 -0600

    Corrección en enum: TipoDocumentoInicial

--- src/main/java/sgpp/modelo/beans/expediente/documentoinicial/TipoDocumentoInicial.java
+++ src/main/java/sgpp/modelo/beans/expediente/documentoinicial/TipoDocumentoInicial.java
@@ -15,9 +15,9 @@
 package sgpp.modelo.beans.expediente.documentoinicial;
 
 public enum TipoDocumentoInicial {
-    CARTA_ACEPTACION,
-    CRONOGRAMA,
-    OFICIO_ASIGNACION,
-    HORARIO_UV,
-    CONSTANCIA_SEGURO
+    CartaAceptacion,
+    Cronograma,
+    OficioAsignacion,
+    HorarioUV,
+    ConstanciaSeguro
 }
